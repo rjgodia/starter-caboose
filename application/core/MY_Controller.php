380f41215c1d4251f6dcc50c1a26b4c3fd3cab83
Clifton Tam <ctam@live.ca>
2016-02-29 13:52:21 -0800

    v2

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -37,6 +37,9 @@ class Application extends CI_Controller {
 		$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
 
 		// finally, build the browser page!
+                $this->data['caboose_styles'] = $this->caboose->styles();
+                $this->data['caboose_scripts'] = $this->caboose->scripts();
+                $this->data['caboose_trailings'] = $this->caboose->trailings();
 		$this->data['data'] = &$this->data;
 		$this->parser->parse('_template', $this->data);
 	}
